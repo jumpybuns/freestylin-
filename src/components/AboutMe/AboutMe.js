be91158ac1ac62f4f4c39a5b69d7a651b7528164
Jumpybuns <ethanpiercepresents@gmail.com>
2021-01-23 22:56:17 -0800

    connected css

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,14 +1,15 @@
+import styles from './AboutMe.css';
 import React, { Component } from 'react';
 
 export default class AboutMe extends Component {
   render() {
     return (
       <div>
-        <div className="Home">
+        <div className={styles.home}>
           Home Sweet Home, <br></br>Feels good doesnt it!<br></br>
           Now lets get to work...
         </div>
-        <div className="Home2">
+        <div className={styles.home2}>
           Go ahead and sign up, <br></br> we can make a list.
         </div>
       </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import styles from './Home.css';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <p className="text1">This is my webpage. total skateboard</p>
+        <p className={styles.text1}>This is my webpage. total skateboard</p>
       </div>
     );
   }
