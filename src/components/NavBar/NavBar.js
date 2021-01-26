d0ff9dcfb6a6f2a7118c62e42af376b62789263e
Jumpybuns <ethanpiercepresents@gmail.com>
2021-01-26 10:45:49 -0800

    sidebar kinda works

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,7 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { SideBarData } from './SideBarData';
 import styles from './NavBar.css';
+import { IconContext } from 'react-icons';
 
 export default function NavBar() {
   const [sidebar, setSidebar] = useState(false);
@@ -12,30 +13,32 @@ export default function NavBar() {
 
   return (
     <>
-      <div className={styles.navbar}>
-        <Link to="#" className={styles.menuBars}>
-          <FaIcons.FaBars onClick={showSidebar} />
-        </Link>
-      </div>
-      <nav className={sidebar ? styles.navMenuActive : styles.navMenu}>
-        <ul className={styles.navMenuItems}>
-          <li className={styles.navbarToggle}>
-            <Link to="#" className={styles.menuBars}>
-              <AiIcons.AiOutlineClose />
-            </Link>
-          </li>
-          {SideBarData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={index.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <IconContext.Provider value={{ color: '#red' }}>
+        <div className={styles.navBar}>
+          <Link to="#" className={styles.menuBars}>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? styles.navMenuActive : styles.navMenu}>
+          <ul className={styles.navMenuItems}>
+            <li className={styles.navbarToggle}>
+              <Link to="#" className={styles.menuBars}>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SideBarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={index.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
     </>
   );
 }
