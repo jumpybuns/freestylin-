fd146ab4496e031781ad28e648b187b6e7160f23
Jumpybuns <ethanpiercepresents@gmail.com>
2021-01-27 13:15:27 -0800

    more gross stuff

--- src/components/CSS2/CSS2.js
+++ src/components/CSS2/CSS2.js
@@ -5,6 +5,9 @@ export default class CSS2 extends Component {
   render() {
     return (
       <div>
+        <span>
+          <p className={styles.text}>hover or tap me</p>
+        </span>
         <p className={styles.text}>hover or tap me</p>
       </div>
     );
